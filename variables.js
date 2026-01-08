554e9a2c1c7a02ce1d93f40e58b55b90f3414473
jared garcia orozco <jaredgarcia45777@gmail.com>
2026-01-08 07:54:31 -0700

    Variables and Constants

---
+++ variables.js
@@ -0,0 +1,12 @@
+// EXAMPLES
+
+// For constants we use const the name the constant and after add value.
+const PI = 3.14;
+
+// For VARIABLES we use let the name the variable and after add value.
+let radius = 3;
+
+let area = PI * radius **2
+
+// To check if it worked go in to inspect view on chrome and check console.
+console.log(area);
