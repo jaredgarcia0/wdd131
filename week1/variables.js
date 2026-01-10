148b282fa1e62ec83420c8537f8c5970d9697969
jared garcia orozco <jaredgarcia45777@gmail.com>
2026-01-10 16:25:08 -0700

    done

--- week1/variables.js
+++ week1/variables.js
@@ -10,3 +10,18 @@ let area = PI * radius **2
 
 // To check if it worked go in to inspect view on chrome and check console.
 console.log(area);
+
+
+const one = 1;
+const two = '2';
+
+
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
+                    
