6f31f20e0437458a9175773db7e31695d74ae012
jared garcia orozco <jaredgarcia45777@gmail.com>
2026-01-21 12:08:20 -0700

    responsive

--- mission/dark-mode.js
+++ mission/dark-mode.js
@@ -1,13 +1,15 @@
 let selectElem = document.getElementById('dark-light-mode');
-let pageContent = document.querySelector('body');
+let logo = document.querySelector('img');
 
-selectElem.addEventListener('change', changeMode);
+selectElem.addEventListener('change', changeTheme);
 
-function changeMode() {
+function changeTheme() {
     let current = selectElem.value;
     if (current === 'Dark') {
         document.body.classList.add('dark-mode');
         document.body.classList.remove('light-mode');
+        document.images.remove('#byui-logo')
+        document.images.add('')
     } else if (current === 'Light'){
         document.body.classList.add('light-mode')
         document.body.classList.remove('dark-mode');
